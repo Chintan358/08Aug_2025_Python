7b225ec5f8b6211837bd27b27398cf1d3923ac28
chintan <chintan@gmail.com>
2025-08-07 12:03:32 +0530

    all

---
+++ 001_Basic/demo.py
@@ -0,0 +1,18 @@
+# #include<stdio.h>
+# #include<conio.h>
+# void main()
+# {
+#     printf("hello world")
+# }
+
+
+# class Demo
+# {
+#     public stait cvoid main(String args[])
+#     {
+#         System.out.println("Hello, world")
+#     }
+# }
+
+
+print("Hello world")
