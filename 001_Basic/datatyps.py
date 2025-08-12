423302ea244c1bb2f1547be9f2d8b29f1798630e
chintan <chintan@gmail.com>
2025-08-12 12:20:07 +0530

    all

---
+++ 001_Basic/datatyps.py
@@ -0,0 +1,39 @@
+
+#numeric
+
+# a = 10
+# b = 10.225
+# c = 4j+5
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# Sequence Data type :
+
+# name = "Tisha"
+# subjects = ["python","java","php"]
+# t = ("a","b","c",1,2,3)
+# #range()
+
+# print(type(name))
+# print(type(subjects))
+# print(type(t))
+
+
+# s = {"test","tech","a","b","a"}
+# m = {"python":"1","java":"2"}
+# print(type(s))
+# print(type(m))
+
+# b = True
+# print(type(b))
+
+
+
+# a = 10
+# b = str(a)
+
+# print(type(b))
+
+
--- 001_Basic/demo.py
+++ 001_Basic/demo.py
@@ -15,4 +15,30 @@
 # }
 
 
-print("Hello world")
+# print("Hello world")
+
+# Hello " python
+
+# print('Hello " python')
+# print("Hello ' Python")
+# print("""Hello
+#     PythonFinalizationError
+#     Hello 
+#     Tops
+# """)
+
+# print("Hello","Java","Hello","Python",sep=" , ")
+
+# print("Hello",end=" ")
+# print("Python")
+
+
+# print("Hello\b \\\\\" python")
+
+# Hello \" python
+
+
+
+a = input("enter number : ")
+print(a)
+
