bd1860d82986768270537de96b17e2c6827feea8
chintan <chintan@gmail.com>
2025-08-14 12:29:09 +0530

    operators

---
+++ 001_Basic/statement.py
@@ -0,0 +1,51 @@
+#conditional
+# if-else
+
+#looping
+#for,while
+
+#control
+# break, continue, pass
+
+# age = 15
+# if age>=18 :
+#     print("elegeble for voting")
+# else:
+#     print("not elegeble...")
+
+
+a  =1000000
+b = 20000
+c = 520
+
+# if a>b:
+#     print("A is greater")
+# else : 
+#     print("B is greater")
+
+# if a>b:
+#     if a>c :
+#         print("A is greater")
+#     else:
+#         print("C is greater")
+# else : 
+#     if b>c :
+#         print("B is greater")
+#     else :
+#         print("C is greater")
+
+
+# if a>b and a>c:
+#     print("A is greater")
+# elif b>a and b>c:
+#     print("B is greater")
+# elif c>a and c>b:
+#     print("c is greater")
+
+# marks = 52
+# 91 - 100 : A
+# 71 : 90 : B
+# 51 : 70 : c
+#35 : 50 : D
+#0 - 35 : F
+# invalid input if out of range (0-100)
