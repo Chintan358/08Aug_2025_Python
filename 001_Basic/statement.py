6e1210c385ca5d7105f2495f2e8708ff06a1fd2b
chintan <chintan@gmail.com>
2025-08-19 12:29:44 +0530

    logical

--- 001_Basic/statement.py
+++ 001_Basic/statement.py
@@ -49,3 +49,40 @@ c = 520
 #35 : 50 : D
 #0 - 35 : F
 # invalid input if out of range (0-100)
+
+
+#for loop
+
+
+# for i in range(15):
+#     print(i)
+
+
+# for i in range(5,15):
+#     print(i)
+
+
+# for i in range(1,15,4):
+#     print(i)
+
+# for i in range(15,1,-2):
+#     print(i)
+
+
+# i = 10
+
+# while i<20:
+#     print(i)
+#     i = i+1
+
+
+# for i in range(15):
+#     if i==5:
+#         # break
+#         continue
+#     print(i)
+
+# if 10<20:
+#     print("20 is greater")
+# else:
+#     pass
---
+++ 002_Logical/primenumber.py
@@ -0,0 +1,30 @@
+# number = 9
+# flag=0
+
+# for i in range(2,number):
+#     if number%i==0:
+#         flag=1
+#         break
+
+# if flag==0:
+#     print("Prime")
+# else:
+#     print("not prime")
+
+
+
+
+for j in range(3,100):
+    number = j
+    flag=0
+
+    for i in range(2,number):
+        if number%i==0:
+            flag=1
+            break
+
+    if flag==0:
+        print(f"{number} : Prime")
+    else:
+        # print(f"{number} : not prime")
+        pass
