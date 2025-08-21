7cb906ea5b9780dbe24605fbc30b7e8b8ce34b3c
chintan <chintan@gmail.com>
2025-08-21 12:22:36 +0530

    logical

---
+++ 002_Logical/factorial.py
@@ -0,0 +1,13 @@
+
+number = 10
+temp = number
+fact = 1
+
+# for i in range(number,0,-1):
+#     fact = fact*i
+
+while number !=0:
+    fact = fact*number
+    number-=1
+
+print(f"factorial of {temp} is  {fact} ")
--- 002_Logical/primenumber.py
+++ 002_Logical/primenumber.py
@@ -14,17 +14,20 @@
 
 
 
-for j in range(3,100):
-    number = j
-    flag=0
+# for j in range(3,100):
+#     number = j
+#     flag=0
 
-    for i in range(2,number):
-        if number%i==0:
-            flag=1
-            break
+#     for i in range(2,number):
+#         if number%i==0:
+#             flag=1
+#             break
 
-    if flag==0:
-        print(f"{number} : Prime")
-    else:
-        # print(f"{number} : not prime")
-        pass
+#     if flag==0:
+#         print(f"{number} : Prime")
+#     else:
+#         # print(f"{number} : not prime")
+#         pass
+
+
+
