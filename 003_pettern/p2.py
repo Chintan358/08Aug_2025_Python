cabd4e5a5b27959c317af78024d4ba86a2139598
chintan <chintan@gmail.com>
2025-08-26 12:26:53 +0530

    init

--- 003_pettern/p2.py
+++ 003_pettern/p2.py
@@ -147,10 +147,10 @@
 # 12345
 
 
-for i in range(5):
-    for j in range(i+1):
-        print(j+1,end="")
-    print()
+# for i in range(5):
+#     for j in range(i+1):
+#         print(j+1,end="")
+#     print()
 
 # 1
 # 23
@@ -158,14 +158,83 @@ for i in range(5):
 # 78910
 # 1112131415
 
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print((i*(i-1))//2 + 1 + j,end="")   
+#     print()
+
+# 0 1 1  0 1
+# 1 2 2  0 2 1 3
+# 2 3 4  0 4 1 5 2 6
+# 3 4 7  0 7 1 8 2 9 3 10
+# 4 5 11 0 11 1 12 2 13 3 14 4 15
+
+# 1 2 4 7 11
+# 1 2 3 4 5
+
 # 5
 # 45
 # 345
 # 2345
 # 12345
 
+# for i in range(5,0,-1):
+#     for j in range(i,6):
+#         print(j,end="")
+#     print()
+
 # 0
 # 10
 # 010
 # 1010
 # 01010
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print((i+j)%2,end="")
+#     print()
+
+
+
+# *****
+#   *
+#   * 
+#   *
+#   *
+
+
+# *   *
+# *   *
+# *****
+# *   *
+# *   *
+
+
+# A
+# BC 
+# DEF 
+# GHIJ
+# KLMNO
+
+# *      *
+# * *  * *
+# *  *   *
+# * *  * *
+# *      *
+
+#     *
+# * * * *
+#         *
+# * * * *
+#     *
+
+
+#     *
+#    * *
+#   *   *
+#   *****
+#   *****
+#   *****
+
+
