82ff7544de4e117756db6a0385976f256fcb8353
chintan <chintan@gmail.com>
2025-09-04 12:33:09 +0530

    all

---
+++ 005_collection/listdemo.py
@@ -0,0 +1,73 @@
+# l = [10,20,30,40,50,50,"Hello",41.56,True]
+# l1 = list((10,20,30,40))
+# print(l)
+# print(len(l))
+# print(l1)
+
+
+l = ["Tisha","Zeel","Devanshi","Ibrahim","Jenil"]
+# print(l[1])
+# print(l[-1])
+# print(l[2:4])
+# print(l[::-1])
+
+# l[1] = "Dhruvin"
+# l[1:3] = ["A"]
+
+# l.insert(1,"Dhruvin")
+# l.append("Dhruvin")
+# k = ["A","1","Hello"]
+# l.extend(k)
+
+# l.remove("Tisha")
+# l.pop(1)
+# l.pop()
+# l.clear()
+# del l
+# print(l)
+
+# for i in l:
+#     print(i)
+
+# for i in range(len(l)):
+#     print(l[i])
+
+# i = 0
+# while i<len(l):
+#     print(l[i])
+#     i+=1
+
+k = []
+
+# for i in l:
+#     if 'e' in i:
+#         k.append(i)
+# print(k)
+
+# k = [i for i in l if "e" in i]
+# k = [i for i in l if i.startswith('T')]
+# k = ["A" for i in l]
+
+# l.sort()
+# l.sort(reverse=True)
+# l.reverse()
+
+# k = sorted(l)
+
+# k = l.copy()
+# k = list(l)
+# k = l[:]
+# k = l
+
+a = [10,20,30,10]
+b = [100,200,300]
+
+# c = a+b
+# print(c)
+
+# a.extend(b)
+# print(a)
+
+# print(a.count(10))
+
+print(a.index(10))
