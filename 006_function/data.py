9e16933cb2c5b5ef98dac267b562d801c9d6fa56
chintan <chintan@gmail.com>
2025-09-16 11:22:56 +0530

    all

---
+++ 006_function/data.py
@@ -0,0 +1,49 @@
+
+# def hello():
+#     print("Hello")
+
+# def square(a):
+#     print(a*a)
+
+# def add(a,b):
+#     print(a+b)
+
+# hello()
+# square(11)
+# add(10,20)
+
+
+# def add(a,b):
+#     return a+b
+
+# def square(a):
+#     return a*a
+
+# k = add(10,20)
+# j = square(k)
+# print(j)
+
+
+# def person(id=0,name="xyz",email="abc@gmail.com"):
+#     print(id,name,email)
+
+
+# person(10,email="tisha@gmail.com")
+
+
+# def add(*a):
+#     print(a)
+
+# add(10,20,30,40,50,60)
+
+# def person(**a):
+#     print(a)
+
+# person(name="devanshi",email="dv@gmail.com")
+
+
+# a = lambda a,b : a+b
+# b = lambda a : a*a
+
+# print(a(10,20))
+# print(b(50))
