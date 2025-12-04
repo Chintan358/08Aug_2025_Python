2dba49e722336744a20bfa3d8e8dce1cc2aab484
chintan <chintan@gmail.com>
2025-12-04 11:55:53 +0530

    crud

--- 015_Djano_Crud/myapp/views.py
+++ 015_Djano_Crud/myapp/views.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from myapp.models import Student
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def register_student(request):
+    id = request.POST.get('id')
     name = request.POST.get('name')
     email = request.POST.get('email')
     age = request.POST.get('age')
 
-    Student.objects.create(name=name, email=email, age=age)
-    return render(request, 'home.html',{'success':'Student registered successfully',})
+    if not id : 
+        Student.objects.create(name=name, email=email, age=age)
+        return render(request, 'home.html',{'success':'Student registered successfully',})
+    else : 
+        st = Student.objects.get(pk=id)
+        st.name = name
+        st.email = email
+        st.age = age
+        st.save()
+        return render(request, 'home.html',{'success':'Student update successfully',})
+
+
+def display(request):
+    students = Student.objects.all()
+    return render(request,"display.html",{"students":students})
+
+def delete_student(request):
+    id =  request.GET.get("id")
+    st = Student.objects.get(pk=id)
+    st.delete()
+    return redirect("display")
+
+def edit_student(request):
+    id =  request.GET.get("id")
+    st = Student.objects.get(pk=id)
+    return render(request,"home.html",{"st":st})
---
+++ 016_django_crud/myapp/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render,redirect
+from myapp.models import *
+# Create your views here.
+def index(request):
+    products = Product.objects.all()
+    if request.method=='POST':
+        name = request.POST['name']
+        price = request.POST['price']
+        qty = request.POST['qty']
+        Product.objects.create(name=name,price=price,qty=qty)
+        return render(request,"index.html",{"products":products})
+    else:   
+        return render(request,"index.html",{"products":products})
+    
+def delete_product(request):
+    id = request.GET['id']
+    prod = Product.objects.get(pk=id)
+    prod.delete()
+    return redirect("index")
+
+def edit_product(request):
+    products = Product.objects.all()
+    if request.method=='POST':
+        id = request.POST['id']
+        name = request.POST['name']
+        price = request.POST['price']
+        qty = request.POST['qty']
+        
+        prod = Product.objects.get(pk=id)
+        prod.name = name
+        prod.price=price
+        prod.qty=qty
+        prod.save()
+        return render(request,"index.html",{"products":products})
+    else:
+        id = request.GET['id']
+        prod = Product.objects.get(pk=id)
+        return render(request,"index.html",{"prod":prod,"products":products})
