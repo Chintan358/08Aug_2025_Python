e66c9932d1be4e4f47d2f33bdb6ccae330d80bce
chintan <chintan@gmail.com>
2026-01-12 09:24:02 +0530

    all

--- 020_Payment/myapp/views.py
+++ 020_Payment/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
-# Create your views here.
+from django.core.mail import send_mail
+from django.conf import settings
+import requests
 import razorpay
 
 def index(request):
@@ -15,3 +17,39 @@ def payment(request):
     payment = client.order.create(data=data) # Amount is in currency subunits.
     
     return JsonResponse(payment)
+
+
+def sendmail(request):
+    context = {}
+  
+    if request.method == 'POST':
+        address = request.POST.get('address')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        if address and subject and message:
+            try:
+                send_mail(subject, message, settings.EMAIL_HOST_USER, [address])
+                context['result'] = 'Email sent successfully'
+            except Exception as e:
+                context['result'] = f'Error sending email: {e}'
+        else:
+            context['result'] = 'All fields are required'
+    
+    return render(request,"mail.html",context)
+
+
+def sendsms(request):
+    
+
+    url = "https://www.fast2sms.com/dev/bulkV2"
+
+    querystring = {"authorization":"APIKEY","message":"This is test message","language":"english","route":"q","numbers":"8866280999"}
+
+    headers = {
+        'cache-control': "no-cache"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    print(response.text)
