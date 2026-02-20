c3d6384a2b3f993ef6a14075468256d35a1e307d
chintan <chintan@gmail.com>
2026-02-20 08:33:46 +0530

    all

--- eshoprest/eshoprest/settings.py
+++ eshoprest/eshoprest/settings.py
@@ -91,12 +91,24 @@ WSGI_APPLICATION = 'eshoprest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eshop',      # Your MySQL database name
+        'USER': 'root',      # Your MySQL username
+        'PASSWORD': 'root',  # Your MySQL password
+        'HOST': 'localhost',       # Or the IP address of your MySQL server
+        'PORT': '3306',            # Default MySQL port
     }
 }
+
 
 
 # Password validation
--- eshoprest/myapp/urls.py
+++ eshoprest/myapp/urls.py
@@ -6,7 +6,13 @@ router = routers.DefaultRouter()
 router.register("users",UserViewSet)
 router.register("categories",CategoryViewSet)
 router.register("products",ProductViewSet)
+router.register("address",AddressViewSet,basename="address")
+router.register("carts",CartViewSet,basename="carts")
 
 urlpatterns = [
     path('', include(router.urls)),
+    path("payment",payment,name="payment"),
+    path("order/create",create_order,name="orders")
+
+
 ]
